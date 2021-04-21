5e70064bc8a4232f17a8e8b9065edd5860ffa288
Lootfi <lotfiholmes@hotmail.com>
2021-04-21 22:27:06 +0100

    validation and sanitization

--- tp3/src/index.js
+++ tp3/src/index.js
@@ -12,7 +12,7 @@ import {
     update_podcast,
     delete_podcast
 } from './utils/database'
-
+import validation from './utils/validation'
 
 
 var app = express()
@@ -27,8 +27,7 @@ app.get('/podcasts/:id', (req, res) => {
 })
 
 app.post('/podcasts',
-    body('status').isString().trim(),
-    body('picture').isURL(),
+    ...validation.create_podcast,
     (req, res) => {
 
         const errors = validationResult(req)
@@ -44,21 +43,43 @@ app.post('/podcasts',
         res.json(new_podcasts).status(201)
     })
 
-app.put('/podcasts/:id', (req, res) => {
-    const id = req.params.id
+app.put('/podcasts/:id',
+    ...validation.update_podcast,
+    (req, res) => {
 
-    const new_podcasts = update_podcast(id, req.body)
+        const errors = validationResult(req)
 
-    res.json(new_podcasts).status(200)
-})
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
 
-app.delete('/podcasts/:id', (req, res) => {
-    const id = req.params.id
+        const id = req.params.id
 
-    const new_podcasts = delete_podcast(id)
+        const new_podcasts = update_podcast(id, req.body)
 
-    res.json(new_podcasts).status(200)
-})
+        res.json(new_podcasts).status(200)
+    })
+
+app.delete('/podcasts/:id',
+    ...validation.delete_podcast,
+    (req, res) => {
+
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const id = req.params.id
+
+        const new_podcasts = delete_podcast(id)
+
+        res.json(new_podcasts).status(200)
+    })
 
 app.listen('8000', () => {
     console.log("express listening on port 8000")
---
+++ tp3/src/utils/validation.js
@@ -0,0 +1,45 @@
+import {
+    body,
+    param,
+    check
+} from 'express-validator'
+
+function check_if_date(string) {
+    return !isNaN(new Date(string).getDate())
+}
+
+export default {
+    create_podcast: [
+        body('name').isString().trim(),
+        body('status').isString().trim(),
+        body('listeners').isNumeric().toInt(),
+        body('picture').isURL(),
+        body('about').isString().trim(),
+        body('releaseDate').custom(value => {
+            return check_if_date(value)
+        }),
+        body('category').isString().trim(),
+        body('hosts').isArray(),
+        check('hosts.*.id').isInt(),
+        check('hosts.*.name').isString().isAlpha("en-US", {
+            "ignore": [" ", "-", "'"]
+        }).trim()
+
+    ],
+    update_podcast: [
+        body('name').optional().isString().trim(),
+        body('status').optional().isString().trim(),
+        body('listeners').optional().isNumeric().toInt(),
+        body('picture').optional().isURL(),
+        body('about').optional().isString().trim(),
+        body('releaseDate').optional().custom(value => {
+            return check_if_date(value)
+        }),
+        body('category').optional().isString().trim(),
+        body('hosts').optional().isArray(),
+        param('id').isString().trim()
+    ],
+    delete_podcast: [
+        param('id').isString().trim()
+    ]
+}
